0dfcfcb84a8b2625d0499b56f5225a718229a523
Yibing Tao <ytao@salesforce.com>
2021-05-25 08:10:25 -0700

    Add comment for TestSignalEnum

--- src/main/java/com/salesforce/bst/extension/TestSignalEnum.java
+++ src/main/java/com/salesforce/bst/extension/TestSignalEnum.java
@@ -3,8 +3,9 @@ package com.salesforce.bst.extension;
 /**
  * @author Yibing Tao
  * Defines test signal type name 
+ * Each test signal enum value represents signals collected by a type of event listener
  */
 public enum TestSignalEnum {
-    AUTOMATION,
-    SELENIUM
+    AUTOMATION, //Test signal collected by Test Provider Listener
+    SELENIUM    // Test signal collected by Selenium Event Listener
 }
