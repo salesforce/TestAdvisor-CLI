78dfce1331713a7554fd19ab41e4a736ae5ef8e7
Yibing Tao <ytao@salesforce.com>
2021-06-08 17:42:29 -0700

    @w-9401830 refactor RestHelper for unit test

---
+++ src/test/java/com/salesforce/cqe/drillbit/ConnectorTest.java
@@ -0,0 +1,38 @@
+package com.salesforce.cqe.drillbit;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.salesforce.cqe.helper.DrillbitException;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.Test;
+
+/**
+ * @author gneumann
+ * @author Yibing Tao
+ */
+public class ConnectorTest {
+
+    @Test(expected = DrillbitException.class)
+	public void testApexPostBadRequest() throws Exception {
+		CloseableHttpClient httpClient = mock(CloseableHttpClient.class);
+        HttpPost httpPost = mock(HttpPost.class);
+        CloseableHttpResponse httpResponse = mock(CloseableHttpResponse.class);
+        StatusLine statusLine = mock(StatusLine.class);
+
+        //and:
+        when(statusLine.getStatusCode()).thenReturn(HttpStatus.SC_BAD_REQUEST);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(httpClient.execute(httpPost)).thenReturn(httpResponse);
+
+        //and:
+        try(Connector connector = new Connector(httpClient,httpPost)){
+            connector.postApex("endpoint", "");
+        }
+	}
+}
