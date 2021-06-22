634ba02821fa6f3500e2a9822fb1f2266f94741f
Yibing Tao <ytao@salesforce.com>
2021-06-21 18:09:47 -0700

    comments update

--- src/main/java/com/salesforce/cqe/helper/DrillbitPortalException.java
+++ src/main/java/com/salesforce/cqe/helper/DrillbitPortalException.java
@@ -2,7 +2,7 @@ package com.salesforce.cqe.helper;
 
 /**
  * @auther Yibing Tao
- * Drillbit Exception will throw when CLI request was rejected by
+ * DrillbitPortalException will throw when CLI request was rejected by
  * drillbit portal. The message will be the portal response.
  */
 public class DrillbitPortalException extends Exception {
--- src/main/java/com/salesforce/cqe/helper/ProcessException.java
+++ src/main/java/com/salesforce/cqe/helper/ProcessException.java
@@ -2,7 +2,7 @@ package com.salesforce.cqe.helper;
 
 /**
  * @author Yibing Tao
- * ProcessException will throw when adapter failed to process the test result
+ * ProcessException will throw when adapter failed to process the test result.
  * The cause will be the actual exception was throw.
  */
 public class ProcessException extends Exception {
