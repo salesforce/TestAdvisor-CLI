27f9e8361d3364d8806dcad0ac6e47ce4e4516c9
Yibing Tao <ytao@salesforce.com>
2021-11-10 11:50:02 -0800

    @w-10021285 add image comparison

---
+++ src/test/java/com/salesforce/cte/testadvisor/ScreenshotManagerTest.java
@@ -0,0 +1,54 @@
+package com.salesforce.cte.testadvisor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+
+import com.github.romankh3.image.comparison.model.ImageComparisonResult;
+import com.github.romankh3.image.comparison.model.ImageComparisonState;
+
+import org.junit.Test;
+
+public class ScreenshotManagerTest {
+
+    @Test
+    public void testScreenshotComparisonSame() throws URISyntaxException{
+        URL baselineUrl = getClass().getClassLoader().getResource("image/login.png");
+        URL currentUrl = getClass().getClassLoader().getResource("image/login.png");
+        File baselineFile = new File(baselineUrl.toURI());
+        File currentFile = new File(currentUrl.toURI());
+        ImageComparisonResult result =  ScreenshotManager.screenshotsComparison(baselineFile, currentFile);
+        assertEquals(ImageComparisonState.MATCH, result.getImageComparisonState());
+    }
+
+    @Test
+    public void testScreenshotComparisonMismatch() throws URISyntaxException{
+        URL baselineUrl = getClass().getClassLoader().getResource("image/login.png");
+        URL currentUrl = getClass().getClassLoader().getResource("image/login2.png");
+        File baselineFile = new File(baselineUrl.toURI());
+        File currentFile = new File(currentUrl.toURI());
+        ImageComparisonResult result =  ScreenshotManager.screenshotsComparison(baselineFile, currentFile);
+        assertEquals(ImageComparisonState.MISMATCH, result.getImageComparisonState());
+        assertTrue(result.getDifferencePercent()>0);
+        assertEquals(1, result.getRectangles().size());
+    }
+
+    @Test
+    public void testScreenshotComparisonSave() throws URISyntaxException, IOException{
+        URL baselineUrl = getClass().getClassLoader().getResource("image/login.png");
+        URL currentUrl = getClass().getClassLoader().getResource("image/login2.png");
+        File baselineFile = new File(baselineUrl.toURI());
+        File currentFile = new File(currentUrl.toURI());
+        File resultFile = new File("result.png");
+        ImageComparisonResult result =  ScreenshotManager.screenshotsComparison(baselineFile, currentFile,resultFile.getAbsolutePath());
+        assertEquals(ImageComparisonState.MISMATCH, result.getImageComparisonState());
+        assertTrue(Files.exists(resultFile.toPath()));
+        Files.delete(resultFile.toPath());
+    }
+    
+}
