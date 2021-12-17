2aa91d94d583377da1c3d8b45f2c9ef0052aeea1
Yibing Tao <ytao@salesforce.com>
2021-12-16 20:03:26 -0800

    @w-10273658 add unit test

--- src/test/java/com/salesforce/cte/testadvisor/ProcessorTest.java
+++ src/test/java/com/salesforce/cte/testadvisor/ProcessorTest.java
@@ -14,12 +14,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
 
 import com.salesforce.cte.adapter.TestAdvisorAdapter;
 import com.salesforce.cte.adapter.TestAdvisorResultAdapter;
+import com.salesforce.cte.adapter.TestAdvisorTestCase;
+import com.salesforce.cte.adapter.TestAdvisorTestSignal;
+import com.salesforce.cte.adapter.TestCaseBase;
 import com.salesforce.cte.adapter.TestNGAdapter;
+import com.salesforce.cte.adapter.TestSignalBase;
 import com.salesforce.cte.datamodel.client.TestRunSignal;
+import com.salesforce.cte.datamodel.client.TestSignal;
 import com.salesforce.cte.datamodel.client.TestStatus;
 import com.salesforce.cte.helper.ProcessException;
 
@@ -79,6 +88,42 @@ public class ProcessorTest{
         assertEquals(TestStatus.PASS, testRunSignal.testExecutions.get(1).status);
         assertEquals("1.1.1", testRunSignal.clientLibraryVersion);
         assertEquals("1.0.1", testRunSignal.clientCliVersion);
+    }
+
+    @Test
+    public void testCompareSameTestCase() throws IOException{
+        TestAdvisorTestCase baseline = createTeseCase(100, 1, "selcmd");
+        TestAdvisorTestCase current = createTeseCase(50, 2, "selcmd");
+        List<TestSignal> signalList = new ArrayList<>();
+
+        assertEquals(100, processor.compareTestCaseExecution(baseline, current, signalList));
+    }
+
+    @Test
+    public void testCompareDiffTestCase() throws IOException{
+        TestAdvisorTestCase baseline = createTeseCase(100, 1, "selcmd1");
+        TestAdvisorTestCase current = createTeseCase(50, 2, "selcmd2");
+        List<TestSignal> signalList = new ArrayList<>();
+
+        assertEquals(0, processor.compareTestCaseExecution(baseline, current, signalList));
+    }
+
+    private TestCaseBase createTeseCase(int secOffset, int num, String seleniumCmd) throws IOException{
+        Path screenshotPath = root.resolve("Screenshots");
+        screenshotPath.toFile().mkdirs();
+        screenshotPath = screenshotPath.resolve(String.valueOf(num)+".png");
+        
+        Files.copy(getClass().getClassLoader().getResourceAsStream("image/login.png"), screenshotPath);
+
+        List<TestAdvisorTestSignal> signalList = new ArrayList<>();
+        TestSignalBase signal = new TestSignalBase("signal name", "signal value", 
+            Instant.now().minusSeconds(100), Level.INFO.toString(), seleniumCmd, "slenium param", "selenium locaor", 
+            num, screenshotPath.toString());
+        signalList.add(signal);
+        TestCaseBase testcase = new TestCaseBase("TestCase",Instant.now().minusSeconds(secOffset), 
+            Instant.now(),com.salesforce.cte.common.TestStatus.PASSED.toString(),signalList);
+
+        return testcase;
     }
 
     @After
