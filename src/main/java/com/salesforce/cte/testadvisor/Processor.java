18f7e01314368ee3bc1e8bcc72e83e280a582373
Yibing Tao <ytao@salesforce.com>
2022-03-18 15:37:13 -0700

    @w-10844818 code clean up

--- src/main/java/com/salesforce/cte/testadvisor/Processor.java
+++ src/main/java/com/salesforce/cte/testadvisor/Processor.java
@@ -24,8 +24,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.github.romankh3.image.comparison.model.ImageComparisonResult;
 import com.github.romankh3.image.comparison.model.ImageComparisonState;
